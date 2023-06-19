33fce44790d75969dc77ea4be57e7a3e1f1948c6
Bendidi Ihab <ihabnobendidi@gmail.com>
2023-06-19 19:18:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,34 @@
-from setuptools import setup, find_packages
-
+from distutils.core import setup
 setup(
-    name='bioval',
-    version='0.1.0',
-    description='A package for Bioinformatics Validation',
-    author='Ihab Bendidi, Ethan Cohen, Auguste Genovesio',
-    packages=find_packages(),
-    install_requires=[
-        'pynvml',
+  name = 'bioval',         # How you named your package folder (MyLib)
+  packages = ['bioval'],   # Chose the same as "name"
+  version = '0.1.1-alpha',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Bioval is a Python package made to provide a wrapper and an easy access to a collection of evaluation metrics for comparing the similarity of two tensors, adapted to different evaluation processes of generative models applied to biological images, and by extension to natural images. The package support unconditional comparison cases, in which we have no labels separating different classes/conditions, and also supports conditional comparisons, with metrics adapted to evaluating the appropriateness of the generated images to all the distributions of real conditions. The package also supports distance of generated conditions from the real negative control condition, used in most biological contexts.',   # Give a short description about your library
+  author = 'Ihab Bendidi, Ethan Cohen, Auguste Genovesio',                   # Type in your name
+  author_email = 'bendidiihab@gmail.Com',      # Type in your E-Mail
+  url = 'https://github.com/IhabBendidi/bioval',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/IhabBendidi/bioval/archive/refs/tags/v0.1.1-alpha.tar.gz',    # I explain this later on
+  keywords = ['biology', 'evaluation', 'generative models',],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
         'numpy',
         'torch',
         'torchvision',
         'Pillow',
         'nvidia-ml-py',
         'piq',
-        'poetry',
-        'scikit-learn',
         'pot',
-    ],
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent',
-    ],
+        'scikit-learn',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
 )
+
